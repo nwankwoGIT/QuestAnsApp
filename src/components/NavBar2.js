aae33cf558901b7fbbbaf749421e0ec1c450d936
nwankwoGIT <61326912+nwankwoGIT@users.noreply.github.com>
2020-08-09 17:53:56 -0500

    Update NavBar2.js

--- src/components/NavBar2.js
+++ src/components/NavBar2.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Menu } from 'semantic-ui-react'
+import client from '../feathers';
 
 const colorsA = ['red', 'orange', 'yellow', 'olive', 'green', 'teal']
 const colorsB = ['blue', 'violet', 'purple', 'pink', 'brown', 'grey']
@@ -7,12 +8,17 @@ const colorsB = ['blue', 'violet', 'purple', 'pink', 'brown', 'grey']
 export default class MenuExampleColoredInverted extends Component {
   state = { activeA: colorsA[0], activeB: colorsB[0] }
 
-  handleAClick = (e, { name }) => this.setState({ activeA: name })
-  handleBClick = (e, { name }) => this.setState({ activeB: name })
+  //handleAClick = (e, { name }) => this.setState({ activeA: name })  
+   handleAClick = (e, { name })  => {
+   this.setState({ activeA: name })    
+   if(name === 'red') {
+      client.logout()
+    }
+  }
+  
 
   render() {
     const { activeA, activeB } = this.state
-
     return (
       <div>
         <Menu inverted>
@@ -22,7 +28,7 @@ export default class MenuExampleColoredInverted extends Component {
               name={c}
               active={activeA === c}
               color={c}
-              onClick={this.handleAClick}
+              onClick={this.handleAClick}  // onClick={() => client.logout()} 
             />
           ))}
         </Menu>        
