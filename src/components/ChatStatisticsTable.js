4195ebcf4a3901a4fd8af8a38c70575a52241a9c
nwankwoGIT <61326912+nwankwoGIT@users.noreply.github.com>
2020-08-09 18:31:41 -0500

    created table file for question display

---
+++ src/components/ChatStatisticsTable.js
@@ -0,0 +1,34 @@
+import React, { Fragment } from 'react';
+import './table.css';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+ export const ChatStatistics = ({questions}) => {   
+   return (
+      <Fragment>
+       <table class="zebra"> 
+          <thead> 
+          <tr> 
+              <th>Who posted question)</th> 
+              <th>Question Text</th> 
+              <th>Date posted</th> 
+              <th>Answers</th>               
+          </tr> 
+         </thead>         
+         {questions.length > 0 ? <Fragment>
+           <tbody>
+          {questions.map(question => (             
+          <tr> 
+            <td> {question.user.email}</td> 
+            <td> {question.text}</td> 
+            <td> {question.createdAt}</td> 
+            {question.answers.map(answer => <td> {answer}<br/></td>)}              
+            
+          </tr>   
+           ))}
+           </tbody>
+          </Fragment> : <p className="text-center bg-gray-100 text-gray-500 py-5">There are currently No Question or Answers in the system</p>} 
+         </table> 
+         </Fragment> 
+    )}
+    
+ 
+
